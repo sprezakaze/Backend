73757d52303a41b1301e22c6573be1600e6c9db9
sprezakaze <8835590@gmail.com>
2023-06-05 19:56:49 +0300

    Создал DTO для категории+списка одежды

---
+++ webapi/Dtos/CategoryClothesDto.cs
@@ -0,0 +1,10 @@
+﻿using webapi.Entities;
+
+namespace webapi.Dtos
+{
+    public class CategoryClothesDto
+    {
+        public Category Category { get; set; }
+        public List<Clothing> Clothings { get; set; }
+    }
+}
