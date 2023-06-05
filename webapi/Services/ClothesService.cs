eeac720aa69a94dd01079de00632106942bc572d
sprezakaze <8835590@gmail.com>
2023-06-05 19:52:07 +0300

    Создал сервис одежды
    Для вывода категорий/одежды по категориям

---
+++ webapi/Services/ClothesService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapi.Dtos;
+using webapi.Entities;
+
+namespace webapi.Services
+{
+    public class ClothesService
+    {
+        private readonly ClothingContext _context;
+        public ClothesService(ClothingContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetCategories() 
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return categories;
+        }
+        public async Task<CategoryClothesDto> GetClothesByCategory(string id)
+        {
+            var categoryUuid = new Guid(id);
+            var category = await _context.Categories.FirstOrDefaultAsync(el => el.Id == categoryUuid) 
+                ?? throw new Exception("Category not found");
+
+            var clothes = await _context.Clothings
+                .Where(el => el.Category.Id == category.Id)
+                .ToListAsync();
+
+            var dto = new CategoryClothesDto { 
+                Category = category, 
+                Clothings = clothes 
+            };
+
+            return dto;
+        }
+    }
+}
