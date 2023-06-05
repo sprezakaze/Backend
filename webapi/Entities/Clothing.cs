904bab32d5154dfe3fbd66896c040bc3a22bf732
sprezakaze <8835590@gmail.com>
2023-06-05 19:48:29 +0300

    Создал сущности базы данных

---
+++ webapi/Entities/Clothing.cs
@@ -0,0 +1,13 @@
+﻿namespace webapi.Entities
+{
+    public class Clothing
+    {
+        public int ClothingId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public Category? Category { get; set; }
+        public int Price { get; set; } 
+        public string? ProductPicture { get; set; }
+        public string? InStock { get; set; } // ?
+    }
+}
---
+++ webapi/Entities/ClothingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Entities
+{
+    public class ClothingContext:DbContext
+    {
+        public ClothingContext(DbContextOptions<ClothingContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Clothing> Clothings { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
