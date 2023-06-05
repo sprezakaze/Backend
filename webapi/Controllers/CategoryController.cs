efa16b1c500134d45627488c4ebcacb63796e58b
sprezakaze <8835590@gmail.com>
2023-06-05 19:53:00 +0300

    Добавил контроллер просмотра по категориям

---
+++ webapi/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Entities;
+using webapi.Services;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ClothesService _clothesService;
+        public CategoryController(ClothesService clothesService)
+        {
+            _clothesService = clothesService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetAll()
+        {
+            return Ok(await _clothesService.GetCategories());
+        }
+        [HttpGet, Route("{id}")]
+        public async Task<ActionResult<List<Category>>> GetClothesByCategory(string id)
+        {
+            return Ok(await _clothesService.GetClothesByCategory(id));
+        }
+    }
+}
