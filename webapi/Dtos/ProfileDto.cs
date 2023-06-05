694db5575c0cf3d00693ca010f1bc47661a9dcc6
sprezakaze <8835590@gmail.com>
2023-06-05 20:01:53 +0300

    Создал DTO профиля

---
+++ webapi/Dtos/ProfileDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using webapi.Entities;
+
+namespace webapi.Dtos
+{
+    public class ProfileDto
+    {
+        [Required]
+        public User User { get; set; } = null!;
+        [Required]
+        public List<CartItem> Cart { get; set; } = null!;
+    }
+}
