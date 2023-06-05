12b34a1be8d20c5427c10e85b28ada0719ed2abf
sprezakaze <8835590@gmail.com>
2023-06-05 20:04:48 +0300

    Создал DTO для позиции в корзине

---
+++ webapi/Dtos/CreateCartItemDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using webapi.Entities;
+
+namespace webapi.Dtos
+{
+    public class CreateCartItemDto
+    {
+        [Required]
+        public int ClothingId { get; set; }
+        [Required, Range(0, 100)]
+        public int Count { get; set; }
+    }
+}
