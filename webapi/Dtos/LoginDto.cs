01ee2a5263c7771cb64416c5e72889ad09ce241a
sprezakaze <8835590@gmail.com>
2023-06-05 20:02:39 +0300

    Создал DTO для авторизации/регистрации

---
+++ webapi/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.Dtos
+{
+    public class LoginDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+    }
+}
---
+++ webapi/Dtos/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.Dtos
+{
+    public class RegisterDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+        [Required, Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
